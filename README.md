62d48f41e4e8bee11cd710cee8fab204fe9a3511
Seth_Hall <ckx5qj@virginia.edu>
2025-12-19 12:35:39 -0500

    Final commit from rivanna after benchmark

--- README.md
+++ README.md
@@ -1,12 +1,42 @@
 # simpix
 
-C++ starter code
-* simpix_start.cpp
-use make to build this example
+## To compile
+In the phys56xx environment, use ```make```, and all neeeded files will be built. the main executable is ```simpix```. 
 
-Usage: simapix_start image1 image2 <output=out.png>
+## Images
+The two images I switched are labeled ```impressionism_1.png``` and ```impressionism_2.png```. The switched images are ```impressionism_1_to_2.png``` and ```impressionism_2_to_1.png```, where the images were transformed 1=>2 and 2=>1, repsectivley. You can also see this in the images ```collage_1_to_2.png``` and ```collage_2_to_1.png```, where the images are displayed as the source (top left), target (top right), and result (bottom left). 
 
-Python starter code
-* simpix_start.py
+## Structure of the script. 
+The executable is in ```simpix.cpp```, and several helper functions, as well as the 'Annealer' class, are in ```Utils.hpp/cpp```. The actual Annealing code, and the function which determines the _difference_ betweeen two pixels, are in ```Utils.hpp/cppo```. Th Annealer class actually does the pixel-swap annealing steps, while the temperature is exponnetially decreased in the ```simpix.cpp``` class. 
 
-Usage: simapix_start image1 image2 <output=out.png>
+## To Execute
+Once you have compiled the ```simpix``` executable, you can run with the following options: 
+
+- ```-h``` Print a message listing possible options, and exit. 
+- ```-t``` Provide a path to the 'target' image 
+- ```-s``` Provice a path to the 'source' image 
+- ```-o``` Provide a path to save the output image
+- ```-c``` Provide a path to save the 'collage' of all 3 images 
+
+## Examples of Execution / Time Needed
+As an example, to transform ```impressionism_2.png``` to ```impressionism_1.png```, you can use: 
+
+``` 
+./simpix -t impressionism_1.png -s impressionism_2.png -o impressionism_2_to_1.png -c collage_2_to_1.png
+
+```bash 
+
+Which takes **47.16 s** to execute with the current annealing parameters. 
+
+Going the other way, you can use: 
+
+``` 
+./simpix -t impressionism_2.png -s impressionism_1.png -o impressionism_1_to_2.png -c collage_1_to_2.png
+
+```bash 
+
+Which takes **44.20 s** to execute. 
+
+
+
+
--- simpix.cpp
+++ simpix.cpp
@@ -16,29 +16,67 @@
 #include "Utils.hpp"
 #include <iostream>
 #include <stdio.h>
+#include <string> 
 using namespace std;
+
+void Usage() 
+{
+  printf(
+    "\n"
+    "Usage: \n"
+    " -h    print this message and exit\n"
+    " -t    specify path to target image\n"
+    " -s    specify path to source image\n"
+    " -o    specify path to output image\n"
+    " -c    specify path to output 'collage' of all three images\n"
+  ); 
+}
 
 int main(int argc, char **argv)
 {
 
-  if (argc<3) {
-    cout << "Usage: simpix_start image1 image2 <output=out.png>" << endl;
-    return 0; 
-  }
-  TString fsrc=argv[1];
-  TString ftgt=argv[2];
-  TString fout;
-  argc>3 ? fout = argv[3] : fout="out.png";
-  cout << "Reading images: source= " << fsrc << " target= " << ftgt << endl;
-  cout << "Output= " << fout << endl;
+  string path_target  = "rotunda_north_facade.png"; 
+  string path_source  = "frisbe_scott_stadium.png"; 
+  string path_output  = "out.png"; 
+  string path_collage = "collage.png"; 
 
+  //get options
+  char opt_flag; 
+  while ((opt_flag = getopt(argc, argv, "ht:s:o:c:")) != -1) {
+
+    switch (opt_flag) {
+      case 'h' : 
+        Usage(); return 0; 
+      case 't' : //path to target image
+        path_target  = string(optarg); break; 
+      case 's' : //path to source image
+        path_source  = string(optarg); break; 
+      case 'o' : //path to output image
+        path_output  = string(optarg); break; 
+      case 'c' : //path to collage
+        path_collage = string(optarg); break; 
+      default  : //unrecognized option
+        std::cerr  << "Unrecognized option '" << opt_flag << "'." << endl;
+        Usage();  
+        return -1;  
+    }
+  }
+
+  printf(
+    "Transforming images.\n"
+    "  target:  %s\n"
+    "  source:  %s\n"
+    "  output:  %s\n"
+    "  collage: %s\n",
+    path_target.data(), path_source.data(), path_output.data(), path_collage.data()
+  );  
 
   TApplication theApp("App", &argc, argv);
 
   // create image objects
-  TASImage *src = new TASImage(fsrc.Data());
-  TASImage *tgt = new TASImage(ftgt.Data());
-  TASImage *out = new TASImage(fsrc.Data()); // start with copy of source
+  TASImage *src = new TASImage(path_source.data());
+  TASImage *tgt = new TASImage(path_target.data());
+  TASImage *out = new TASImage(path_source.data()); // start with copy of source
 
   //create the Annealer class. this is defined in 'Utils.hpp' and 'Utils.cpp'
   // the annealer class will automatically check that the 'target' and 'source' images have the same dimensions.
@@ -54,20 +92,27 @@ int main(int argc, char **argv)
   // 'square error' between the new and old arrangement of pixels; i.e., 
   // (red1-red2)^2 + (blue1-blue2)^2 + (green1-green2)^2
   const double T_start = 400.; 
-  const double T_end   = 0.1; //T_start/1000.; 
+  const double T_end   = 0.1;
 
   cout << endl; 
   unsigned long swaps_per_T = width*height*8; //gives, on average, 50 swaps-per-pixel 
 
-  int n_T_steps = 40; 
-  const double T_inflation = pow( T_end/T_start, 1./((double)n_T_steps)); 
+  //this is the number of times temperature will be decreased. 
+  // the temperature decreases exponentially, so that the final temperature will be 'T_end'. 
+  int n_T_steps = 30; 
+  const double T_inflation = pow( T_end/T_start, 1./((double)n_T_steps-1)); 
 
-  vector<double> pts_T, pts_diff; 
+  //track the annealing temperature + total 'difference' per pixel 
+  //vector<double> pts_T, pts_diff; 
 
-  auto t_start = chrono::steady_clock::now(); 
+  //starting temperature 
   double T = T_start;
-  for (int step=0; step<n_T_steps; step++) {
   
+  //start the timer clock 
+  auto t_start = chrono::steady_clock::now(); 
+  for (int step=0; step<n_T_steps; step++) {
+    
+    //do the alloted number of steps for this T-value, and measure the time it takes 
     auto t_step_start = chrono::steady_clock::now(); 
     double fraction_accepted = annealer.Swap_update(T, swaps_per_T); 
     //double diff = annealer.Difference_per_pixel(); 
@@ -92,7 +137,7 @@ int main(int argc, char **argv)
   printf("Total time elapsed: %.2f s", total_duration.count()/1.e3); cout << endl; 
 
   // print the results
-  TCanvas *c1 = new TCanvas("c1", "images", 640, 480);
+  TCanvas *c1 = new TCanvas("c1", "images", 1920, 1080);
   c1->Divide(2,2);
 
   c1->cd(1);
@@ -102,7 +147,7 @@ int main(int argc, char **argv)
   tgt->Draw("X");
   c1->cd(3);
   out->Draw("X");
-  c1->Print("collage.png");
+  c1->Print(path_collage.data());
   
   /*TCanvas *c2 = new TCanvas("c2", "annealing schedule", 640, 480); 
   auto g = new TGraph(pts_T.size(), pts_T.data(), pts_diff.data()); 
@@ -111,12 +156,11 @@ int main(int argc, char **argv)
   g->Draw(); */ 
 
   //use this little hack to get the output image to be saved as the proper size
-  TCanvas *c_output = new TCanvas("c_img", "image", 1920, 1080); 
+  TCanvas *c_output = new TCanvas("c_img", "image", width, height); 
   gPad->SetMargin(0.,0.,0.,0.); 
   out->Draw("X"); 
-  c_output->SaveAs(fout.Data()); 
+  c_output->SaveAs(path_output.data()); 
   delete c_output; 
-
 
   // coment out the lines for running in batch mode
   cout << "Press ^c to exit" << endl;
